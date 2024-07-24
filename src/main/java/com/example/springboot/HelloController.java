3ad91603eab14ffdfe2039421b55c6a950f9d97a
Mae Large <mlarge@broadcom.com>
2024-07-24 13:53:38 -0700

    ivs 6

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from SupplyChainV2, ivs test!";
+		return "Greetings from SupplyChainV2, ivs test 6!";
 	}
 
 }
--- src/test/java/com/example/springboot/HelloControllerTest.java
+++ src/test/java/com/example/springboot/HelloControllerTest.java
@@ -21,11 +21,11 @@ class HelloControllerTest {
 
     @Test
     void index() throws Exception {
-        assertEquals("Greetings from SupplyChainV2, ivs test!", controller.index());
+        assertEquals("Greetings from SupplyChainV2, ivs test 6!", controller.index());
 
         mockMvc
             .perform(get("/"))
             .andExpect(status().isOk())
-            .andExpect(content().string("Greetings from SupplyChainV2, ivs test!"));
+            .andExpect(content().string("Greetings from SupplyChainV2, ivs test 6!"));
     }
 }
