d83876f2a40952704e0cc90463d65b591ef067b6
Mae Large <mlarge@broadcom.com>
2024-07-25 07:50:31 -0700

    hook at 15 minReplica

--- src/test/java/com/example/springboot/HelloControllerTest.java
+++ src/test/java/com/example/springboot/HelloControllerTest.java
@@ -21,11 +21,11 @@ class HelloControllerTest {
 
     @Test
     void index() throws Exception {
-        assertEquals("Greetings from SupplyChainV2, watch party tektonhook15!", controller.index());
+        assertEquals("Greetings from SupplyChainV2, watch party tektonhook15 scanmeagain!", controller.index());
 
         mockMvc
             .perform(get("/"))
             .andExpect(status().isOk())
-            .andExpect(content().string("Greetings from SupplyChainV2, watch party tektonhook15!"));
+            .andExpect(content().string("Greetings from SupplyChainV2, watch party tektonhook15 scanmeagain!"));
     }
 }
